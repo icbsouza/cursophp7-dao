28147a53c6d86fe59c876ce422802fcfa81b552e
Ilton Carlos Barbosa de Souza <ilton_carlos@hotmail.com>
2018-03-29 20:11:07 -0300

    incluindo o insert e update

--- index.php
+++ index.php
@@ -14,16 +14,27 @@
 		// echo $root;
 	
 	// Lista todos os usuario em ordem crescente por login
-		//$lista = Usuario::getList();
-		//echo json_encode($lista);
+		// $lista = Usuario::getList();
+		// echo json_encode($lista);
 
 	// lista todos a partir de um like.
 		//$search = Usuario::search('r');
 		//echo json_encode($search);
 
 	// Testando o login e a senha
+		//$usuario = new Usuario();
+		//$usuario->login("mauricio", "5431231");
+		//echo $usuario;
+
+	// Inserindo no banco a partir de um Storage Procedure criada no banco "sp_usarios_insert";
+		// $aluno = new Usuario("aluno","@lun0");
+		// $aluno->insert();
+		// echo $aluno;
+
+	// 
 		$usuario = new Usuario();
-		$usuario->login("mauricio", "5431231");
+		$usuario->loadbyId(8);
+		$usuario->update("Professor", "31#$%");
 		echo $usuario;
 
 ?>
